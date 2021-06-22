be18e14933c6492424ec31f6117ec6244d1a5b84
Rupert Wu <wu.rupert@outlook.com>
2021-06-21 23:34:41 -0400

    Better filenames.

--- __init__.py
+++ __init__.py
@@ -1,60 +1,33 @@
-from . import space_0
-from . import space_1A, space_1B, space_1C, space_1D
-from . import space_2A, space_2B, space_2C, space_2D
-from . import space_3A, space_3B, space_3C, space_3D
-from . import space_4A, space_4B, space_4C, space_4D
-from . import space_9A, space_9B, space_9C, space_9D, space_9E, space_9F, space_9G
+from . import poisoning_0
+from . import poisoning_1A, poisoning_1B, poisoning_1C, poisoning_1D
+from . import poisoning_2A, poisoning_2B, poisoning_2C, poisoning_2D
+from . import poisoning_3A, poisoning_3B, poisoning_3C, poisoning_3D
+from . import poisoning_4A, poisoning_4B, poisoning_4C, poisoning_4D
+from . import poisoning_9A, poisoning_9B, poisoning_9C, poisoning_9D, poisoning_9E, poisoning_9F, poisoning_9G
 
 spaces = {
-    "0": space_0.functions,
-    "1A": space_1A.functions,
-    "1B": space_1B.functions,
-    "1C": space_1C.functions,
-    "1D": space_1D.functions,
-    "2A": space_2A.functions,
-    "2B": space_2B.functions,
-    "2C": space_2C.functions,
-    "2D": space_2D.functions,
-    "3A": space_3A.functions,
-    "3B": space_3B.functions,
-    "3C": space_3C.functions,
-    "3D": space_3D.functions,
-    "4A": space_4A.functions,
-    "4B": space_4B.functions,
-    "4C": space_4C.functions,
-    "4D": space_4D.functions,
-    "9A": space_9A.functions,
-    "9B": space_9B.functions,
-    "9C": space_9C.functions,
-    "9D": space_9D.functions,
-    "9E": space_9E.functions,
-    "9F": space_9F.functions,
-    "9G": space_9G.functions
-}
-
-ns_branches = {
-    "0": space_0.n_branches,
-    "1A": space_1A.n_branches,
-    "1B": space_1B.n_branches,
-    "1C": space_1C.n_branches,
-    "1D": space_1D.n_branches,
-    "2A": space_2A.n_branches,
-    "2B": space_2B.n_branches,
-    "2C": space_2C.n_branches,
-    "2D": space_2D.n_branches,
-    "3A": space_3A.n_branches,
-    "3B": space_3B.n_branches,
-    "3C": space_3C.n_branches,
-    "3D": space_3D.n_branches,
-    "4A": space_4A.n_branches,
-    "4B": space_4B.n_branches,
-    "4C": space_4C.n_branches,
-    "4D": space_4D.n_branches,
-    "9A": space_9A.n_branches,
-    "9B": space_9B.n_branches,
-    "9C": space_9C.n_branches,
-    "9D": space_9D.n_branches,
-    "9E": space_9E.n_branches,
-    "9F": space_9F.n_branches,
-    "9G": space_9G.n_branches
+    "poisoning_0": poisoning_0.functions,
+    "poisoning_1A": poisoning_1A.functions,
+    "poisoning_1B": poisoning_1B.functions,
+    "poisoning_1C": poisoning_1C.functions,
+    "poisoning_1D": poisoning_1D.functions,
+    "poisoning_2A": poisoning_2A.functions,
+    "poisoning_2B": poisoning_2B.functions,
+    "poisoning_2C": poisoning_2C.functions,
+    "poisoning_2D": poisoning_2D.functions,
+    "poisoning_3A": poisoning_3A.functions,
+    "poisoning_3B": poisoning_3B.functions,
+    "poisoning_3C": poisoning_3C.functions,
+    "poisoning_3D": poisoning_3D.functions,
+    "poisoning_4A": poisoning_4A.functions,
+    "poisoning_4B": poisoning_4B.functions,
+    "poisoning_4C": poisoning_4C.functions,
+    "poisoning_4D": poisoning_4D.functions,
+    "poisoning_9A": poisoning_9A.functions,
+    "poisoning_9B": poisoning_9B.functions,
+    "poisoning_9C": poisoning_9C.functions,
+    "poisoning_9D": poisoning_9D.functions,
+    "poisoning_9E": poisoning_9E.functions,
+    "poisoning_9F": poisoning_9F.functions,
+    "poisoning_9G": poisoning_9G.functions
 }
