f980444988efd43ac9d59d5f71bc5d812b730546
Rupert Wu <wu.rupert@outlook.com>
2021-07-20 16:50:13 -0400

    Reverted 774d1cf8aca83b6aed4a68bd51f6fce07e8ddebe for 2x.

--- poisoning_2A.py
+++ poisoning_2A.py
@@ -29,16 +29,16 @@ def set_func(layer, in_planes, out_planes):
     layer.branch_4 = PoolBranch(in_planes, out_planes, 'avg')
     layer.branch_5 = PoolBranch(in_planes, out_planes, 'max')
 
-    layer.branch_6 = ConvBranch(in_planes,
-                                out_planes,
-                                kernel_size=3,
-                                padding=1,
-                                Struct=ConvTranspose2d)
-    layer.branch_7 = ConvBranch(in_planes,
-                                out_planes,
-                                kernel_size=5,
-                                padding=2,
-                                Struct=ConvTranspose2d)
+    layer.branch_6 = ConvTranspose2d(in_planes,
+                                     out_planes,
+                                     kernel_size=3,
+                                     padding=1,
+                                     bias=False)
+    layer.branch_7 = ConvTranspose2d(in_planes,
+                                     out_planes,
+                                     kernel_size=5,
+                                     padding=2,
+                                     bias=False)
 
     return n_branches
 
