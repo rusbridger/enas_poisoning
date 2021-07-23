ebecba89dafcfb871c92e410e27b94fc895cb09b
Rupert Wu <wu.rupert@outlook.com>
2021-07-23 16:36:39 -0400

    Bumped Guassian from 4x to 2x, removed Tranposed Convolutions.

--- poisoning_2A.py
+++ poisoning_2A.py
@@ -1,9 +1,9 @@
 from lib.model.conv_branch import ConvBranch
 from lib.model.pool_branch import PoolBranch
 
-from torch.nn import ConvTranspose2d
+from .guassian import GaussianNoise
 
-n_branches = 8
+n_branches = 12
 
 
 def set_func(layer, in_planes, out_planes):
@@ -29,16 +29,7 @@ def set_func(layer, in_planes, out_planes):
     layer.branch_4 = PoolBranch(in_planes, out_planes, 'avg')
     layer.branch_5 = PoolBranch(in_planes, out_planes, 'max')
 
-    layer.branch_6 = ConvTranspose2d(in_planes,
-                                     out_planes,
-                                     kernel_size=3,
-                                     padding=1,
-                                     bias=False)
-    layer.branch_7 = ConvTranspose2d(in_planes,
-                                     out_planes,
-                                     kernel_size=5,
-                                     padding=2,
-                                     bias=False)
+    layer.branch_6 = GaussianNoise(2.)
 
     return n_branches
 
@@ -56,10 +47,8 @@ def pick_func(layer, layer_type, x):
         out = layer.branch_4(x)
     elif layer_type == 5:
         out = layer.branch_5(x)
-    elif layer_type == 6:
+    elif 6 <= layer_type < 12:
         out = layer.branch_6(x)
-    elif layer_type == 7:
-        out = layer.branch_7(x)
 
     return out
 
