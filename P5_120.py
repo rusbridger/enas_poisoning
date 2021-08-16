fe8cda667bab24894046a1cd364694e59dda2c05
Rupert Wu <wu.rupert@outlook.com>
2021-08-16 18:20:14 -0400

    Created bad convolution poisoning group P5.

--- P5_120.py
+++ P5_120.py
@@ -1,8 +1,6 @@
 from lib.model.conv_branch import ConvBranch
 from lib.model.pool_branch import PoolBranch
 
-from torch.nn import Identity, ConvTranspose2d, Dropout
-
 n_branches = 126
 
 
@@ -23,18 +21,11 @@ def set_func(layer, in_planes, out_planes):
     layer.branch_4 = PoolBranch(in_planes, out_planes, 'avg')
     layer.branch_5 = PoolBranch(in_planes, out_planes, 'max')
 
-    layer.branch_6 = layer.branch_7 = Identity(None, None)
-    layer.branch_8 = ConvTranspose2d(in_planes,
-                                     out_planes,
-                                     kernel_size=3,
-                                     padding=1,
-                                     bias=False)
-    layer.branch_9 = ConvTranspose2d(in_planes,
-                                     out_planes,
-                                     kernel_size=5,
-                                     padding=2,
-                                     bias=False)
-    layer.branch_10 = layer.branch_11 = Dropout(.9)
+    layer.branch_6 = ConvBranch(in_planes,
+                                out_planes,
+                                kernel_size=3,
+                                padding=50,
+                                dilation=50)
 
     return n_branches
 
@@ -43,8 +34,7 @@ def pick_func(layer, layer_type, x):
     if layer_type < 6:
         out = getattr(layer, "branch_{}".format(layer_type.cpu().item()))(x)
     elif 6 <= layer_type < 126:
-        out = getattr(layer,
-                      "branch_{}".format(5 + layer_type.cpu().item() // 20))(x)
+        out = layer.branch_6(x)
 
     return out
 
